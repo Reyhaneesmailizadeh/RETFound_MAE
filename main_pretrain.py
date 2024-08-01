bc7f8d15c4a173a6d50dcb159c29ae6632c07aa3
Reyhane Esmailizadeh <109366727+Reyhaneesmailizadeh@users.noreply.github.com>
2024-08-01 13:10:37 +0330

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -210,6 +210,8 @@ def main(args):
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
             data_loader_valid.sampler.set_epoch(epoch)
+        if len(data_loader_valid.dataset) == 0:
+            print("Warning1: validation dataset is empty.")
         train_stats, loss_train, loss_valid = train_one_epoch(
             model, data_loader_train, data_loader_valid,
             optimizer, device, epoch, loss_scaler,
