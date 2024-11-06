1a271e6439861b64f9a969f6f4bb6412b512fcc6
Reyhane Esmailizadeh <109366727+Reyhaneesmailizadeh@users.noreply.github.com>
2024-11-06 12:07:30 +0330

    Update misc.py

--- util/misc.py
+++ util/misc.py
@@ -286,7 +286,7 @@ def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
     return total_norm
 
 
-def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
+def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler, fold = None):
     output_dir = Path(args.output_dir)
     epoch_name = str(epoch)
     if loss_scaler is not None:
@@ -303,7 +303,7 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
             save_on_master(to_save, checkpoint_path)
     else:
         client_state = {'epoch': epoch}
-        model.save_checkpoint(save_dir=args.task, tag="checkpoint-best", client_state=client_state)
+        model.save_checkpoint(save_dir=args.task, tag=f"checkpoint-best-{fold}", client_state=client_state)
 
 
 def save_model_pretrain(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
