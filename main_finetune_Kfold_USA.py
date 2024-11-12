c06183a9e5ea9dc50324bb4c700e71c76c726b3a
Reyhane Esmailizadeh <109366727+Reyhaneesmailizadeh@users.noreply.github.com>
2024-11-12 11:14:31 +0330

    Update main_finetune_Kfold_USA.py

--- main_finetune_Kfold_USA.py
+++ main_finetune_Kfold_USA.py
@@ -173,9 +173,9 @@ def main(args):
     kf = KFold(n_splits=N_FOLDS, shuffle=True, random_state=args.seed)
     for fold, (train_idx, val_idx) in enumerate(kf.split(dataset_full)):
         
-        # print("Validation Set Filenames for Fold {}: ".format(fold + 1))
-        # for idx in val_idx:
-        #     filename = dataset_full[idx].filename  # Adjust according to your dataset's structure
+        print("Validation Set Filenames for Fold {}: ".format(fold + 1))
+        for idx in val_idx:
+            filename = dataset_full[idx].filename  # Adjust according to your dataset's structure
             
         print(filename)
         dataset_train = torch.utils.data.Subset(dataset_full, train_idx)
