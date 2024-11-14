8ff72423fe9ffabc8931c5e3251cb67d6a47fb17
Reyhane Esmailizadeh <109366727+Reyhaneesmailizadeh@users.noreply.github.com>
2024-11-14 11:16:23 +0330

    Update misc.py

--- util/misc.py
+++ util/misc.py
@@ -354,7 +354,11 @@ def load_model(args, model_without_ddp, optimizer, loss_scaler):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
+            
+        # manually initialize fc layer
+        trunc_normal_(model_without_ddp.head.weight, std=2e-5)
         model_without_ddp.load_state_dict(checkpoint['model'], strict = False)
+        
         print("Resume checkpoint %s" % args.resume)
         if 'optimizer' in checkpoint and 'epoch' in checkpoint and not (hasattr(args, 'eval') and args.eval):
             optimizer.load_state_dict(checkpoint['optimizer'])
